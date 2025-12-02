a1d54003592862aa527175465ab1421203d6a9cb
solankimansi2808@gmail.com <mernimishas@gmail.com>
2025-12-02 10:38:05 +0530

    extend payload limit for upload file

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
 // CORS for Replit
 app.use(cors({
